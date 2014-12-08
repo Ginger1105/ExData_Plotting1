a1b86566ebb7c266b17ae631c55da1d8b8633226
Ginger1105 <jiangmingdong1105@outlook.com>
2014-12-08 20:54:45 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,7 +1,4 @@
-> data=read.table("household_power_consumption.txt",sep=";",skip=66637,nrows=2880)
-> png("plot1.png")
-> hist(data$V3,col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
-> dev.off()
-null device 
-          1 
-> 
+data=read.table("household_power_consumption.txt",sep=";",skip=66637,nrows=2880)
+png("plot1.png")
+hist(data$V3,col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+dev.off()
