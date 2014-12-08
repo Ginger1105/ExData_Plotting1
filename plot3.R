731bf6c78dbcda5ce6209cb71e15f625b587b6f1
Ginger1105 <jiangmingdong1105@outlook.com>
2014-12-08 20:55:59 +0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,8 +1,7 @@
-> png("plot3.png")
-> plot(data$dt,data$V7,xlab=NA,ylab="Energy sub metering",type="l")
-> lines(data$dt,data$V8,xlab=NA,ylab="Energy sub metering",type="l",col="red")
-> lines(data$dt,data$V9,xlab=NA,ylab="Energy sub metering",type="l",col="blue")
-> legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
-> dev.off()
-null device 
-          1 
+data=read.table("household_power_consumption.txt",sep=";",skip=66637,nrows=2880)
+png("plot3.png")
+plot(data$dt,data$V7,xlab=NA,ylab="Energy sub metering",type="l")
+lines(data$dt,data$V8,xlab=NA,ylab="Energy sub metering",type="l",col="red")
+lines(data$dt,data$V9,xlab=NA,ylab="Energy sub metering",type="l",col="blue")
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
+dev.off()
